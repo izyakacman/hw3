ccea25f9d39407ef69d7e08d937af0aa2b971eb8
aliensol <aliensol@mail.ru>
2021-05-10 22:30:09 +0300

    []

--- main.cpp
+++ main.cpp
@@ -94,9 +94,9 @@ int main()
 	print_ip(char{ -1 }); // 255 
 	print_ip(short{ 0 }); // 0.0 
 	print_ip(int{ 2130706433 }); // 127.0.0.1 
-	print_ip(long long{ 8875824491850138409 }); // 123.45.67.89.101.112.131.41 
+	print_ip( long{ 8875824491850138409 }); // 123.45.67.89.101.112.131.41 
 	print_ip(string{ "Hello, World!" }); // Hello, World! 
 	//print_ip(vector<int>{100, 200, 300, 400}); // 100.200.300.400 
 	//print_ip(list<short>{400, 300, 200, 100}); // 400.300.200.100 
-	print_ip(make_tuple(123, 456, 789, 0));  // 123.456.789.0
+	//print_ip(make_tuple(123, 456, 789, 0));  // 123.456.789.0
 }
