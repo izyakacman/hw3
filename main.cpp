bda8894e6fc743a6c8e7583d7dfc85f4cb0c40b6
izyakacman <58343884+izyakacman@users.noreply.github.com>
2021-05-11 10:27:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ print_ip(T ip)
 /**
 *	Вывод IP адреса из контейнера vector или list
 *	Проверка на наличие в типе Т функции emplace, в обоих контейнерах она есть
-*//*
+*/
 template<typename T>
 typename enable_if<is_member_function_pointer<decltype(&T::emplace<>)>::value>::type
 print_ip(T ip)
@@ -47,7 +47,7 @@ print_ip(T ip)
 	}
 
 	cout << endl;
-}*/
+}
 
 /**
 *	Вывод IP адреса из объекта string
@@ -96,7 +96,7 @@ int main()
 	print_ip(int{ 2130706433 }); // 127.0.0.1 
 	print_ip( long{ 8875824491850138409 }); // 123.45.67.89.101.112.131.41 
 	print_ip(string{ "Hello, World!" }); // Hello, World! 
-	//print_ip(vector<int>{100, 200, 300, 400}); // 100.200.300.400 
+	print_ip(vector<int>{100, 200, 300, 400}); // 100.200.300.400 
 	//print_ip(list<short>{400, 300, 200, 100}); // 400.300.200.100 
 	//print_ip(make_tuple(123, 456, 789, 0));  // 123.456.789.0
 }
